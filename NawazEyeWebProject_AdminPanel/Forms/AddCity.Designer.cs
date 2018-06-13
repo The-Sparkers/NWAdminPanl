c402b6aaaaf7003d1da84db01239ee5933b7d868
Umair Tahir <professor.ut.786@gmail.com>
2018-06-13 13:41:56 +0500

    AddCityFormCompleted

--- NawazEyeWebProject_AdminPanel/Forms/AddCity.Designer.cs
+++ NawazEyeWebProject_AdminPanel/Forms/AddCity.Designer.cs
@@ -28,8 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDelCharges = new System.Windows.Forms.MaskedTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -50,11 +55,46 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Delivery Charges:";
             // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(78, 9);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(148, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtDelCharges
+            // 
+            this.txtDelCharges.BeepOnError = true;
+            this.txtDelCharges.Location = new System.Drawing.Point(110, 42);
+            this.txtDelCharges.Mask = "RS.000";
+            this.txtDelCharges.Name = "txtDelCharges";
+            this.txtDelCharges.Size = new System.Drawing.Size(49, 20);
+            this.txtDelCharges.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(84, 81);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // toolTip
+            // 
+            this.toolTip.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Error;
+            this.toolTip.ToolTipTitle = "Required";
+            // 
             // AddCity
             // 
+            this.AcceptButton = this.btnSave;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(238, 116);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtDelCharges);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "AddCity";
@@ -68,5 +108,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.MaskedTextBox txtDelCharges;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ToolTip toolTip;
     }
 }
--- NawazEyeWebProject_AdminPanel/Forms/AddCity.cs
+++ NawazEyeWebProject_AdminPanel/Forms/AddCity.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NawazEyeWebProject.Models;
+using NawazEyeWebProject_AdminPanel.ProjectClasses;
+using System;
 using System.Windows.Forms;
 
 namespace NawazEyeWebProject_AdminPanel.Forms
@@ -15,6 +10,54 @@ namespace NawazEyeWebProject_AdminPanel.Forms
         public AddCity()
         {
             InitializeComponent();
+        }
+        private bool Save()
+        {
+            if (CheckAllRequired())
+            {
+                decimal d = Convert.ToDecimal(txtDelCharges.Text.Substring(3));
+                City city = new City(txtName.Text, d);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void Reset()
+        {
+            txtName.Text = "";
+            txtDelCharges.Text = "";
+        }
+        private bool CheckAllRequired()
+        {
+            if (txtName.Text == "")
+            {
+                toolTip.Show("This feild is required", txtName);
+                return false;
+            }
+            if (txtDelCharges.Text == "RS.")
+            {
+                toolTip.Show("This feild is required", txtDelCharges);
+                return false;
+            }
+            return true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Save())
+                {
+                    Reset();
+                    MessageBoxes.Success("Record Added Successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBoxes.Error(ex.Message);
+            }
         }
     }
 }
