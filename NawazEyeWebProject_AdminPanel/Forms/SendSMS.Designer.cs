a34cb2a785ad1d1d0aebcec54e27825ccf4ee46a
Umair Tahir <professor.ut.786@gmail.com>
2018-07-01 17:32:37 +0500

    Interface to send Email and SMS

--- NawazEyeWebProject_AdminPanel/Forms/SendSMS.Designer.cs
+++ NawazEyeWebProject_AdminPanel/Forms/SendSMS.Designer.cs
@@ -28,19 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSendSms = new System.Windows.Forms.Button();
+            this.btnSendEmail = new System.Windows.Forms.Button();
+            this.lblCharCount = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Write Message:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.AcceptsReturn = true;
+            this.txtMessage.AcceptsTab = true;
+            this.txtMessage.Location = new System.Drawing.Point(16, 30);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(333, 369);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // btnSendSms
+            // 
+            this.btnSendSms.Location = new System.Drawing.Point(274, 405);
+            this.btnSendSms.Name = "btnSendSms";
+            this.btnSendSms.Size = new System.Drawing.Size(75, 23);
+            this.btnSendSms.TabIndex = 2;
+            this.btnSendSms.Text = "Send SMS";
+            this.btnSendSms.UseVisualStyleBackColor = true;
+            this.btnSendSms.Click += new System.EventHandler(this.btnSendSms_Click);
+            // 
+            // btnSendEmail
+            // 
+            this.btnSendEmail.Location = new System.Drawing.Point(193, 405);
+            this.btnSendEmail.Name = "btnSendEmail";
+            this.btnSendEmail.Size = new System.Drawing.Size(75, 23);
+            this.btnSendEmail.TabIndex = 3;
+            this.btnSendEmail.Text = "Send Email";
+            this.btnSendEmail.UseVisualStyleBackColor = true;
+            this.btnSendEmail.Click += new System.EventHandler(this.btnSendEmail_Click);
+            // 
+            // lblCharCount
+            // 
+            this.lblCharCount.AutoSize = true;
+            this.lblCharCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCharCount.Location = new System.Drawing.Point(16, 405);
+            this.lblCharCount.Name = "lblCharCount";
+            this.lblCharCount.Size = new System.Drawing.Size(14, 13);
+            this.lblCharCount.TabIndex = 4;
+            this.lblCharCount.Text = "0";
             // 
             // SendSMS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(361, 437);
+            this.Controls.Add(this.lblCharCount);
+            this.Controls.Add(this.btnSendEmail);
+            this.Controls.Add(this.btnSendSms);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label1);
             this.Name = "SendSMS";
-            this.Text = "Send SMS";
+            this.Text = "Send Message";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSendSms;
+        private System.Windows.Forms.Button btnSendEmail;
+        private System.Windows.Forms.Label lblCharCount;
     }
 }
--- NawazEyeWebProject_AdminPanel/Forms/SendSMS.cs
+++ NawazEyeWebProject_AdminPanel/Forms/SendSMS.cs
@@ -1,20 +1,88 @@
-﻿using System;
+﻿using NawazEyeWebProject_AdminPanel.ProjectClasses;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NawazEyeWebProject_AdminPanel.Forms
 {
     public partial class SendSMS : Form
     {
-        public SendSMS(List<string> BuyerName, List<string> BuyerNumber)
+        List<CustomerContactInfo> customers = new List<CustomerContactInfo>();
+        public SendSMS(List<CustomerContactInfo> customers)
         {
             InitializeComponent();
+            this.customers = customers;
+        }
+        private void SendSms(string message, string number)
+        {
+            //This function will send the sms
+        }
+        private void SendEmail(string message, string email)
+        {
+            //This function will send the Email
+        }
+
+        private void btnSendSms_Click(object sender, EventArgs e)
+        {
+            if (txtMessage.Text != "")
+            {
+                if (txtMessage.Text.Length < 140)
+                {
+                    string message;
+                    try
+                    {
+                        foreach (var customer in customers)
+                        {
+                            message = "Dear " + customer.Name + ",\n" + txtMessage.Text + "\nRegrads: Eyeworld Team";
+                            SendSms(message, customer.PhoneNumber);
+                        }
+                        MessageBoxes.Success("SMS(s) sent succesfully");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBoxes.Error(ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBoxes.Error("Messsage Body is too Large.");
+                }
+            }
+            else
+            {
+                MessageBoxes.Info("Message Body is empty.");
+            }
+        }
+
+        private void btnSendEmail_Click(object sender, EventArgs e)
+        {
+            if (txtMessage.Text != "")
+            {
+                string message;
+                try
+                {
+                    foreach (var customer in customers)
+                    {
+                        message = "Dear " + customer.Name + ",\n" + txtMessage.Text + "\n\nRegrads,\nEyeworld Team";
+                        SendEmail(message, customer.Email);
+                    }
+                    MessageBoxes.Success("Email(s) sent succesfully");
+                }
+                catch (Exception ex)
+                {
+                    MessageBoxes.Error(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBoxes.Error("Message Body is empty.");
+            }
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            lblCharCount.Text = ""+txtMessage.Text.Length;
+            
         }
     }
 }
